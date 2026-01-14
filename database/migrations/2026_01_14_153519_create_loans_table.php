ae2f7616b3966512f74852a4508f565d19292edf
salsa17708 <salsabila17708@gmail.com>
2026-01-14 22:41:21 +0700

    feat: create loans table with relationships to users, books, and members

--- database/migrations/2026_01_14_153519_create_loans_table.php
+++ database/migrations/2026_01_14_153519_create_loans_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('loans', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete(); // Relasi ke User (Petugas/Peminjam)
+            $table->foreignId('book_id')->constrained('books')->cascadeOnDelete(); // Relasi ke Buku
+            $table->foreignId('member_id')->constrained('members')->cascadeOnDelete(); // Relasi ke Anggota
+            $table->date('tanggal_pinjam');
+            $table->date('tanggal_kembali')->nullable(); // Nullable karena awal pinjam belum kembali
+            $table->enum('status', ['dipinjam', 'kembali'])->default('dipinjam');
             $table->timestamps();
         });
     }
