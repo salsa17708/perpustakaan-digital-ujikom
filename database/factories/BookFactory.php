a09307730778b699648aa71bfff273c439338004
salsa17708 <salsabila17708@gmail.com>
2026-01-15 00:09:48 +0700

    feat: implement default state for Book model in BookFactory

--- database/factories/BookFactory.php
+++ database/factories/BookFactory.php
@@ -15,9 +15,13 @@ class BookFactory extends Factory
      * @return array<string, mixed>
      */
     public function definition(): array
-    {
-        return [
-            //
-        ];
-    }
+{
+    return [
+        'judul' => fake()->sentence(3), // Buat judul acak 3 kata
+        'penulis' => fake()->name(),    // Buat nama orang acak
+        'penerbit' => fake()->company(), // Buat nama perusahaan acak
+        'tahun_terbit' => fake()->year(), // Buat tahun acak
+        'stok' => fake()->numberBetween(0, 50), // Angka 0 s/d 50
+    ];
+}
 }
