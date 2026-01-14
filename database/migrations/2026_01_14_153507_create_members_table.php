87bdf1194c5ee4f332a3c268dfcbf6ed33ebeda4
salsa17708 <salsabila17708@gmail.com>
2026-01-14 22:40:12 +0700

    feat: create members table with unique NISN, name, address, and phone number fields

--- database/migrations/2026_01_14_153507_create_members_table.php
+++ database/migrations/2026_01_14_153507_create_members_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('members', function (Blueprint $table) {
             $table->id();
+            $table->string('nisn')->unique(); // Identitas unik siswa
+            $table->string('nama');
+            $table->text('alamat');
+            $table->string('nomor_telepon');
             $table->timestamps();
         });
     }
