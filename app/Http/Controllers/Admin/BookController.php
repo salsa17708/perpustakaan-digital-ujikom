1cf89b1a821772af77d5ff31037ad8e39f493fc5
salsa17708 <salsabila17708@gmail.com>
2026-01-15 13:17:12 +0700

    feat: update namespace in BookController to align with admin structure

--- app/Http/Controllers/Admin/BookController.php
+++ app/Http/Controllers/Admin/BookController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
+use App\Http\Controllers\Controller;
 use App\Models\Book;
 use Illuminate\Http\Request;
 use Inertia\Inertia; // <--- Wajib ada
